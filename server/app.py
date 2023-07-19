7d12bd34c889793a42b8058e19a77c7d3f98478f
Nikhil Date <nikhil.s.date@gmail.com>
2023-07-19 14:30:13 +0530

    start adding get_stats methods

--- server/app.py
+++ server/app.py
@@ -21,25 +21,24 @@ def home():
     return json_util.dumps(mongo.db.games.find({}))
 
 
-# schema:
-# collection games:
-# "first_player_id": string (can be 'ai')
-# "second_player_id": string (can be 'ai')
-# "moves": string
-# "result": integer (0 is draw by full board, 1 is draw by agreement, 2 is first player wins by position, 3 is first player wins by resignation, 4 is second player wins by position, 5 is second player wins by resignation"
-# "guild_id": string
-# "datetime": date and time object
-
-# collection player
-# "player_id": string
-# "rating": (somehow store time series)
-
 db_manager = DBManager(app)
+
 
 @app.route("/add-game", methods=['POST'])
 def add_game():
     new_game = request.get_json()
-    _id = db_manager.add_game(new_game['first_player_id'], new_game['second_player_id'], new_game['moves'], new_game['result'], datetime.fromisoformat(new_game['datetime']))
+    _id = db_manager.add_game(new_game['first_player_id'], new_game['second_player_id'], new_game['moves'],
+                              new_game['result'], datetime.fromisoformat(new_game['datetime']))
     return json_util.dumps(db_manager.get_game(_id.inserted_id))
+
+
+@app.route("/stats/<player_id: string>", methods=['GET'])
+def get_stats(player_id):
+    other_player_id = request.args.get('other_player_id')
+    guild_id = request.args.get('guild_id')
+    from_date = datetime.fromisoformat(request.args.get('from_date'))
+    to_date = datetime.fromisoformat(request.args.get('to_date'))
+    return json_util.dumps(db_manager.get_stats(player_id, other_player_id, guild_id, from_date, to_date))
+
 
 app.run(ssl_context='adhoc')
--- server/db/db.py
+++ server/db/db.py
@@ -7,15 +7,31 @@ from bson import ObjectId
 from pymongo.cursor import Cursor
 
 
+# schema:
+# collection games:
+# "first_player_id": string (can be 'ai')
+# "second_player_id": string (can be 'ai')
+# "moves": string
+# "result": integer (0 is draw by full board, 1 is draw by agreement, 2 is first player wins by position, 3 is first player wins by resignation, 4 is second player wins by position, 5 is second player wins by resignation"
+# "guild_id": string
+# "datetime": date and time object
+
+# collection player
+# "player_id": string
+# "rating": (somehow store time series)
+
 class DBManager:
 
     def __init__(self, app: Flask):
         self.db = PyMongo(app).db
 
-    def add_game(self, first_player_id: str, second_player_id: str, moves: list, result: int, dtime: datetime):
+    def add_game(self, first_player_id: str, second_player_id: str, guild_id: str, channel_id: str, moves: list,
+                 result: int, dtime: datetime):
         doc = {
             "first_player_id": first_player_id,
             "second_player_id": second_player_id,
+            "guild_id": guild_id,
+            "channel_id": channel_id,
             "moves": moves,
             "result": result,
             "dtime": dtime
@@ -25,3 +41,44 @@ class DBManager:
 
     def get_game(self, game_id) -> Cursor:
         return self.db.games.find({"_id": ObjectId(game_id)})
+
+    def get_stats(self, player_id: str, other_player_id=None, guild_id: str = None, from_date: datetime = None,
+                  to_date: datetime = None):
+
+        pipeline = []
+        pipeline.extend(
+            {
+                "$match": {"$or": [{"first_player_id": player_id}, {"second_player_id": player_id}]}
+            }
+        )
+        if other_player_id is not None:
+            pipeline.extend(
+                {
+                    "$match": {"$or": [{"first_player_id": other_player_id}, {"second_player_id": other_player_id}]}
+                }
+            )
+        if guild_id is not None:
+            pipeline.extend(
+                {
+                    "$match": {"guild_id": guild_id}
+                }
+            )
+        if from_date is not None:
+            pipeline.extend(
+                {
+                    "$match": {"datetime": {"$gte": from_date}}
+                }
+            )
+        if to_date is not None:
+            pipeline.extend(
+                {
+                    "$match": {"datetime": {"$lte": to_date}}
+                }
+            )
+        pipeline.extend([
+            {
+                "$group": {"_id": "$result", "count": {"$sum": 1}}
+            }
+        ])
+
+        return self.db.games.aggregate(pipeline)
