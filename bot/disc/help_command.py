5d6420052a813a92a9c38ac800f1278de72f6416
Nikhil Date <nikhil.s.date@gmail.com>
2023-08-02 16:32:18 +0530

    Edit bot help message and newgame command help

--- bot/disc/help_command.py
+++ bot/disc/help_command.py
@@ -19,13 +19,13 @@ class MyHelp(MinimalHelpCommand):
             names = [self.context.clean_prefix + c.name for c in commands]
             if names:
                 embed.add_field(name="All commands", value="\n".join(names), inline=False)
-        embed.add_field(name='', value="Use `!help [command]` for more info on a command."
-                                       "\n\nIn command help, angle brackets indicate a required parameter. "
+        embed.add_field(name='', value="Use `!help [command]` for more info on a command.")
+        embed.add_field(name='', value="In command help, angle brackets indicate a required parameter. "
                                        "Square brackets indicate an optional parameter. "
-                                       "A value for an optional parameter, if given, indicates the default value for that parameter. "
-                                       "For example, 'rows=6' means that the default value for the 'rows' parameter is 6. "
-                                       "Only the values of parameters should be specified when running commands, not the names. "
-                                       "See the example usage for each command for examples of this.")
+                                       "\nA value for an optional parameter, if given, indicates the default value for that parameter. "
+                                       "For example, `rows=6` in the help for the `newgame` command means that the default value for the `rows` parameter is `6`. "
+                                       "\nOnly the values of parameters should be specified when running commands, not the names. "
+                        , inline=False)
         await self.get_destination().send(embed=embed)
 
     # async def send_command_help(self, command: Command[Any, ..., Any], /) -> None:
